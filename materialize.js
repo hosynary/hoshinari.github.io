ffdd8793fba5fb550e721a9da84eabdbb6391bac
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-11-21 01:40:10 +0900

    Add picture and footer

---
+++ materialize.js
@@ -0,0 +1,28 @@
+/**
+ * Returns a function, that, when invoked, will only be triggered at most once
+ * during a given window of time. Normally, the throttled function will run
+ * as much as it can, without ever going more than once per `wait` duration;
+ * but if you'd like to disable the execution on the leding edge, pass
+ * `{leading: false}`. To disable execution on the trailing edge, ditto.
+ * @license https://raw.github.com/jashkenas/underscore/master/LICENSE
+ * @param {function} func
+ * @param {number} wait
+ * @param {Object=} options
+ * @returns {Function}
+ */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
