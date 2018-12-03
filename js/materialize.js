a1e00ecf1699b6ea7af38efa7bd0bf06736a8a47
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-12-03 09:27:28 +0900

    Add scroll

--- js/materialize.js
+++ js/materialize.js
@@ -10,3 +10,45 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+$(function() {
+  // スクロールのオフセット値
+  var offsetY = -10;
+  // スクロールにかかる時間
+  var time = 500;
+
+  // ページ内リンクのみを取得
+  $('a[href^="#"]').click(function() {
+    // 移動先となる要素を取得
+    var target = $(this.hash);
+    if (!target.length) return ;
+    // 移動先となる値
+    var targetY = target.offset().top+offsetY;
+    // スクロールアニメーション
+    $('html,body').animate({scrollTop: targetY}, time, 'swing');
+    // ハッシュ書き換えとく
+    window.history.pushState(null, null, this.hash);
+    // デフォルトの処理はキャンセル
+    return false;
+  });
+
+  var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+
