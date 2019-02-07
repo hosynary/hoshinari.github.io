b9805c238856f3733f0b1f200432a06c2fa7516a
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2019-02-07 23:38:27 +0900

    Add font animation

--- js/myself.js
+++ js/myself.js
@@ -2,30 +2,30 @@
 
 $(function() {
 
-  $('#hobby').fadeOut(1);
-  $('#work').fadeOut(1);
-  $('#name').fadeOut(1);
-  $('#recent').fadeOut(1);
-  $('#histry').fadeOut(1);
+  // $('#hobby').fadeOut(1);
+  // $('#work').fadeOut(1);
+  // $('#name').fadeOut(1);
+  // $('#recent').fadeOut(1);
+  // $('#histry').fadeOut(1);
 
 
   setTimeout(function(){
     $('#work').fadeIn(500);
-  },200);
+  },500);
 
   setTimeout(function(){
     $('#hobby').fadeIn(500);
-  },700);
+  },1000);
 
   setTimeout(function(){
     $('#histry').fadeIn(500);
-  },1200);
+  },1500);
   
   setTimeout(function(){
     $('#recent').fadeIn(500);
-  },1700);
+  },2000);
 
   setTimeout(function(){
     $('#name').fadeIn(500);
-  },2200);
+  },2500);
 });
